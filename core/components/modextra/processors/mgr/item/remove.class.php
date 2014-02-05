244e6974bb448b73c52aaa8f60be9aeddd98bb3a
Maxim Shkalin <dt5@bk.ru>
2014-02-06 00:23:20 +0400

    Initial commit

---
+++ core/components/modextra/processors/mgr/item/remove.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Remove an Item
+ */
+class modExtraItemRemoveProcessor extends modObjectRemoveProcessor {
+    public $checkRemovePermission = true;
+    public $objectType = 'modExtraItem';
+    public $classKey = 'modExtraItem';
+    public $languageTopics = array('modextra');
+}
+
+return 'modExtraItemRemoveProcessor';
